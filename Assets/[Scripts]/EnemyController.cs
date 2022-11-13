ac6e4a991253be76914e13571036722dd5995f92
MaybeCanadian <1coffeye@gmail.com>
2022-11-13 18:27:23 -0500

    added simple enemy

---
+++ Assets/[Scripts]/EnemyController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [Header("Movement Properties")]
+    public float horizontalSpeed;
+    public Transform groundPoint;
+    public Transform aheadPoint;
+    public Transform frontPoint;
+    public float groundRadius;
+    public LayerMask groundLayerMask;
+    public bool isGrounded;
+    public bool isGroundAhead;
+    public bool isObstacleAhead;
+    public Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Vector2.left;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isObstacleAhead = Physics2D.Linecast(groundPoint.position, frontPoint.position, groundLayerMask);
+        isGroundAhead = Physics2D.Linecast(groundPoint.position, aheadPoint.position, groundLayerMask);
+
+        isGrounded = Physics2D.OverlapCircle(groundPoint.position, groundRadius, groundLayerMask);
+
+        if(isGrounded && isGroundAhead)
+        {
+            Move();
+        }
+        if(isGrounded && (!isGroundAhead || isObstacleAhead))
+        {
+            Flip();
+        }
+    }
+
+    public void Move()
+    {
+        transform.position += new Vector3(direction.x * horizontalSpeed * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    public void Flip()
+    {
+        var x = transform.localScale.x * -1.0f;
+        direction *= -1.0f;
+        transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(groundPoint.position, groundRadius);
+
+        Gizmos.DrawLine(groundPoint.position, frontPoint.position);
+        Gizmos.DrawLine(groundPoint.position, aheadPoint.position);
+    }
+}
